74fc5716950a990029d1d590c6a84ad104ae15d9
Sébastien DROUARD <seb.drouard@gmail.com>
2022-10-05 15:29:16 +0200

    Added testsFeat.py for feature testing purposes

--- testsFeat.py
+++ testsFeat.py
@@ -1,8 +1,17 @@
 import yaml
+from jproperties import Properties
 
-with open(r'./properties/test.yaml') as file:
+configs = Properties()
+
+with open(r'properties/stats_jira.yaml') as file:
     # The FullLoader parameter handles the conversion from YAML
     # scalar values to Python the dictionary format
     testlist = yaml.load(file, Loader=yaml.FullLoader)
 
-    print(testlist.get('jira.user'))
+    print("- open with pyyaml :", testlist.get('jira.user'))
+
+with open('properties/stats_jira.properties', 'rb') as config_file:
+    configs.load(config_file)
+
+    print("- open with jproperties :", configs.get('showProperties').data)
+
